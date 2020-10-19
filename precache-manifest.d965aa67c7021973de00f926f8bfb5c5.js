0b8c85fe21ad022f6ab93b5436d57b66c254076d
birukbelay <belay.bura@gmail.com>
2020-10-19 22:35:20 +0300

    Updates

---
+++ precache-manifest.d965aa67c7021973de00f926f8bfb5c5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4a6a7be49be9340ef3fbc9e1806921",
+    "url": "/newGemenaye/index.html"
+  },
+  {
+    "revision": "a28728f52dfb1f342de8",
+    "url": "/newGemenaye/static/css/3.938589fe.chunk.css"
+  },
+  {
+    "revision": "27cca5f8cdd2799a7f09",
+    "url": "/newGemenaye/static/css/main.c09242b1.chunk.css"
+  },
+  {
+    "revision": "020edd2d0b4f0e0f32b3",
+    "url": "/newGemenaye/static/js/2.ac9a641a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/newGemenaye/static/js/2.ac9a641a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28728f52dfb1f342de8",
+    "url": "/newGemenaye/static/js/3.5ecafa5a.chunk.js"
+  },
+  {
+    "revision": "10f7d8adbc150eaf7cb6",
+    "url": "/newGemenaye/static/js/4.8075c9f0.chunk.js"
+  },
+  {
+    "revision": "27cca5f8cdd2799a7f09",
+    "url": "/newGemenaye/static/js/main.19b652c1.chunk.js"
+  },
+  {
+    "revision": "084b2cd100b096b10853",
+    "url": "/newGemenaye/static/js/runtime-main.99f69657.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/newGemenaye/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/newGemenaye/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/newGemenaye/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/newGemenaye/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/newGemenaye/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/newGemenaye/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/newGemenaye/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/newGemenaye/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/newGemenaye/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/newGemenaye/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/newGemenaye/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/newGemenaye/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/newGemenaye/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/newGemenaye/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
